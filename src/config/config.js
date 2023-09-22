c9badd1dfadf53114e28710d2f869d51fb9ca4b7
sivamw6 <105441569+sivamw6@users.noreply.github.com>
2023-09-22 17:56:14 +1000

    config set up

---
+++ src/config/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  // PORT ENV
+  port: process.env.PORT,
+
+
+  // Database ENVs
+
+
+  // Auth ENVs
+}
--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 // Set the instance for the server
 const express = require('express');
 const morgan = require('morgan');
+require('dotenv').config();
 
 // Local imports
+const config = require('./config/config');
 const routes = require('./routes/routes');
 const ApiError = require('./utils/ApiError');
 const ApiErrorHandler = require('./middleware/apiErrorHandler');
+
+
+// Dev debug console logs
+const debugStartup = require('debug')('app:startup');
 
 // Init express app
 const app = express(); 
@@ -14,6 +20,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(morgan('dev')); // morgan is a logger for requests, it will offer more details in the console
+debugStartup("Parsing middleware enabled on all routes");
 
 // Establish routes
 app.use('/api', routes())
@@ -27,8 +34,8 @@ app.use((req, res, next) => {
 app.use(ApiErrorHandler);
 
 // Port to listen on
-const PORT = process.env.PORT || 3000;
+const port = config.port;
 app.listen(
-  PORT, 
-  () => console.log(`Server started on port: ${PORT}`)
+  port, 
+  () => console.log(`Server started on port: ${port}`)
 );
