a15bb063e9d356b0826c4d7bb8d95e3c87d40266
sivamw6 <105441569+sivamw6@users.noreply.github.com>
2023-09-25 15:29:32 +1000

    Created authController and auth Routes

--- src/config/db.js
+++ src/config/db.js
@@ -6,11 +6,21 @@ try {
   dbStartup("Connecting to the database...");
   var serviceAccount = require("/Users/Mavis/Documents/Holmesglen/firebase/bb-app-service-account-key.json");
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
+    credential: admin.credential.cert(serviceAccount),
+    storageBucket: "gs://bb-app-b64be.appspot.com"
   });
 
   // Connection to the DB is established = can run queries
   dbStartup("Connected to the database");
+  const db = admin.firestore();
+  const bucket = admin.storage().bucket();
+
+  // Export these db methods
+  module.exports = {
+    db,
+    bucket
+  }
+
 } catch (error) {
   debugError500(error);
 }
