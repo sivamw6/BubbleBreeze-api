5a76d6de222a1d397bee1ac365b39c1435b75530
sivamw6 <105441569+sivamw6@users.noreply.github.com>
2023-10-05 13:47:24 +1100

    Creatred get all product in controller

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -3,7 +3,41 @@ const ApiError = require('../utils/ApiError');
 
 
 module.exports = {
+  // Get all users
   async getAllProducts(req, res, next) {
-    res.send("Test hehehe")
-  }
+
+    try {
+  
+      const productsRef = db.collection('products');
+      const snapshot = await productsRef.get();
+
+      if (snapshot.empty) {
+        return next(ApiError.badRequest("No Products!!"));
+      }
+  
+      let products = [];
+      snapshot.forEach(doc => {
+        products.push({
+          id: doc.id,
+          name: doc.data().name,
+          description: doc.data().description,
+          image: doc.data().image,
+          category: doc.data().category,
+          size: doc.data().size,
+          texture: doc.data().texture,
+          price: doc.data().price,
+          onSale: doc.data().onSale,
+          isAvailable: doc.data().isAvailable,
+        })
+      })
+  
+      res.send(products);
+      
+
+
+    } catch (error) {
+      return next(ApiError.internal('The products have gone missing', error));
+    }
+  },
+
 }
